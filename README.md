879fe5be7e960c668e2c6b8332f4006c40606f69
Shweta Mandavgane <shwetamandavgane@gmail.com>
2020-07-30 10:46:43 -0700

    Added SNS topic to upload-portfolio-lambda.py

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ This is my professional portfolio. It uses AWS and React
 Chocolatey
 Git & github
 SSH
+AWS S3
+AWS Lambda with Python and boto3
+AWS CodeBuild
+AWS CodePipeline
+AWS SNS
 HTML
 CSS
 Font Awesome
--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,19 +1,33 @@
+import json
 import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_bucket = s3.Bucket('portfolio.shwetamandavgane.net')
-build_bucket = s3.Bucket('springboot-codepipelines3bucket')
+def lambda_handler(event, context):
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuilds',portfolio_zip)
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj,nm,ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    try:
+        topic = sns.Topic('arn:aws:sns:us-east-1:400010474826:PortfolioDeployment')
+        portfolio_bucket = s3.Bucket('portfolio.shwetamandavgane.net')
+        build_bucket = s3.Bucket('springboot-codepipelines3bucket')
+
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuilds',portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm,ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        topic.publish(Subject="Portfolio Deployment Successful",Message="Portfolio was deployed successfully")
+    except:
+        topic.publish(Subject="Portfolio Deployment Failed",Message="Portfolio updated deployment failed ")
+        raise
+
+    return "Files uploaded"
